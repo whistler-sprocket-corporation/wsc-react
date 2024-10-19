d0475c7311df80cda63dc63d6a0838e674b8397a
Michael B <mjbradvica@gmail.com>
2024-10-19 18:52:59 -0400

    test coverage and styling

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,13 @@ import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    coverage: {
+      enabled: true,
+      include: ["./src/**"],
+      thresholds: {
+        branches: 100,
+      },
+    },
     environment: "jsdom",
     exclude: [...configDefaults.exclude],
     globals: true,
